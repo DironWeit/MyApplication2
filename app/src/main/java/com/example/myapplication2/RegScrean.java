408863cc058a4e14bf064a8aa15f4ca614c18ad3
DironWeit <arseneiantonov27@gmail.com>
2025-11-19 14:46:03 +0300

    3

---
+++ app/src/main/java/com/example/myapplication2/RegScrean.java
@@ -0,0 +1,15 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegScrean extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aaction_teg_screan);
+
+    }
+}
