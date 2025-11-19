413af8b9491f99ed312ee6ff691543b44261726c
DironWeit <arseneiantonov27@gmail.com>
2025-11-19 14:46:03 +0300

    3

---
+++ app/src/main/java/com/example/myapplication2/ScrollingActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication2;
+
+import android.os.Bundle;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.example.myapplication2.databinding.ActivityScrollingBinding;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    private ActivityScrollingBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityScrollingBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        Toolbar toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
+        toolBarLayout.setTitle(getTitle());
+
+        //FloatingActionButton fab = binding.appBar;
+        //fab.hide();
+    }
+}
